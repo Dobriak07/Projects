0cc70f423f92b6888219f04b06657b3ac078eed9
PavelP <bigkot1989@gmail.com>
2022-08-01 11:38:33 +0300

    01.08.22

--- 1/dist/1.js
+++
@@ -1,9 +0,0 @@
-import inquirer from "inquirer";
-let prompt = new inquirer.ui.BottomBar();
-for (let i = 1; i < 10; i++) {
-    setTimeout(() => {
-        prompt.updateBottomBar('');
-        prompt.updateBottomBar(`${i}`);
-    }, 500);
-}
-//# sourceMappingURL=1.js.map
--- 1/src/1.ts
+++
@@ -1,9 +0,0 @@
-import inquirer from "inquirer";
-
-let prompt = new inquirer.ui.BottomBar();
-
-for (let i = 1; i < 10; i++) {
-    setTimeout(() => {
-        prompt.updateBottomBar(`${i}`)
-    }, 500);
-}
